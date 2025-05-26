79132395d52adb2b1021f7df3eabf0ac4a265e81
Pedro Henrique Marques <rocha.pedrohenrique91@gmail.com>
2025-05-26 07:55:43 -0300

    Substituindo o projeto antigo pelo projeto correto

---
+++ src/test/java/io/github/pubsub/rabbitmq/rabbitmqproject/RabbitMqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pubsub.rabbitmq.rabbitmqproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
